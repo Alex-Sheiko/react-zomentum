978ca24ed25a27b500e1d78370897bc1db0b4cc0
Alex Sheiko <82119411+by-ultra-dev@users.noreply.github.com>
2023-03-13 10:19:34 +0300

    init: creat project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import { ReactComponent as LogoIcon } from '../../assets/icons/logo.svg';
+import './styles.scss';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <div className="header__container container">
+          <a className="header__logo" href="/">
+            <LogoIcon />
+          </a>
+          <nav className="header__nav">
+            <a className="header__link header__link--active" href="/">
+              Features
+            </a>
+            <a className="header__link" href="/">
+              Blog
+            </a>
+            <a className="header__link" href="/">
+              Pricing
+            </a>
+            <a className="header__link" href="/">
+              Company
+            </a>
+          </nav>
+          <button className="header__btn">Get a demo</button>
+        </div>
+      </div>
+    </header>
+  );
+};
